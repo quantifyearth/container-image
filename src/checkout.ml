a0f8b58c85191c1a6fdd17b263374ad523d0155e
Thomas Gazagnaire <thomas@gazagnaire.org>
2023-11-05 10:58:45 +0100

    Update dependencies

--- src/checkout.ml
+++ src/checkout.ml
@@ -1,16 +1,30 @@
 open Container_image_spec
 
+let ( let* ) x f = match x with Ok x -> f x | Error e -> Error e
+let ( let+ ) x f = match x with Ok x -> Ok (f x) | Error e -> Error e
+let sizes = [| "B"; "KiB"; "MiB"; "GiB"; "TiB"; "PiB"; "EiB"; "ZiB"; "YiB" |]
+
+let bytes_to_size ?(decimals = 2) ppf = function
+  | 0L -> Format.fprintf ppf "0 byte"
+  | n ->
+      let n = Int64.to_float n in
+      let i = Float.floor (Float.log n /. Float.log 1024.) in
+      let r = n /. Float.pow 1024. i in
+      Format.fprintf ppf "%.*f %s" decimals r sizes.(int_of_float i)
+
 let checkout_layer ~sw ~cache layer =
-  let file = Cache.Blob.get_fd ~sw cache layer in
-  let layer = Flow.with_gzip file in
-  Fmt.epr "XXX LIST START\n%!";
-  let files = Tar_eio.Archive.list layer in
-  Fmt.epr "XXX LIST OK\n%!";
-  List.iter
-    (fun file ->
-      let file = file.Tar.Header.file_name in
-      Fmt.epr "XXXX FILE=%s\n%!" file)
-    files
+  let fd = Cache.Blob.get_fd ~sw cache layer in
+  let fd = Tar_eio_gz.of_source fd in
+  let c = ref 0 in
+  Tar_eio_gz.fold
+    (fun _hdr () ->
+      incr c;
+      if !c mod 100 = 0 then Printf.printf ".%!"
+        (* Format.printf "%s (%s, %a)\n%!" hdr.Tar.Header.file_name
+           (Tar.Header.Link.to_string hdr.link_indicator)
+           (bytes_to_size ~decimals:2)
+           hdr.Tar.Header.file_size; *))
+    fd ()
 
 let checkout_manifest ~sw ~cache m =
   let layers = Manifest.Docker.layers m in
